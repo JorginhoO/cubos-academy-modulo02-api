5bd8f590c9fea91784f27eb038973b48d3f8b1bb
JorginhoO <jorge.infoo88@gmail.com>
2023-08-23 23:04:19 -0300

    query  parameters

--- src/arrays.js
+++ src/arrays.js
@@ -1,8 +1,8 @@
-const professor = [
+const professores = [
     { id: 1, nome: 'Guido', stack: 'Back-end' },
     { id: 2, nome: 'Dani', stack: 'Front-end' },
     { id: 3, nome: 'Diego', stack: 'Front-end' },
     { id: 4, nome: 'Vidal', stack: 'Back-end' }
 ];
 
-module.exports = professor;
+module.exports = professores;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 const express = require('express'); // importador  servidor Http
 const professores = require('../src/arrays');
-const professor = require('../src/arrays');
 
 const app = express();
 
@@ -8,17 +7,27 @@ app.get('/', (req, resp) => {
    resp.send('Hello World!! This is my first server...')  //resposta verbo get Http 
 });
 
+//localhost:3000/professores
 app.get('/professores', (req, resp) => {
-   resp.send(professor);
+   const { stack } = req.query;  // paramentro de consulta
+   let resultado = professores;
+
+   if (stack) {
+      resultado = professores.filter((professor) => {
+         return professor.stack === stack;
+      });
+   }
+   resp.send(resultado);
 });
 
+//localhost:3000/professores/4
 app.get('/professores/:id', (req, resp) => {
-   const professorId = professor.find((professores) => {
-      return professores.id === Number(req.params.id);
+   const professorId = professores.find((professor) => {
+      return professor.id === Number(req.params.id); // paramentros de rotas
    });
-
    resp.send(professorId);
-
 });
+
+
 
 app.listen(3000); //porta do servidor
